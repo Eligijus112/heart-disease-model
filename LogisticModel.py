3ab3000e204e2157f966d2b784e9469d3b8a07f4
Eligijus <elasorama@gmail.com>
2021-02-23 08:02:28 +0200

    Added a model tuning workflow for logistic regression

--- LogisticModel.py
+++ LogisticModel.py
@@ -5,7 +5,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import RepeatedKFold
 
 # Precision and recall scores
-from sklearn.metrics import recall_score, precision_score
+from sklearn.metrics import recall_score, precision_score, roc_auc_score
 
 # Data wrangling 
 import pandas as pd 
@@ -35,39 +35,83 @@ rkf = RepeatedKFold(
 # Loading the hyper parameters
 hp = conf.get("LogisticRegressionHP")
 
-# Placeholders
-recalls, precisions = [], []
+# Placeholder 
+results = pd.DataFrame({})
 
-# Iterating over the dataset 
-for train_index, test_index in rkf.split(X):
-    # Getting the train and test splits 
-    X_train, X_test = X.iloc[train_index], X.iloc[test_index]
-    y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+for c in hp.get('C'):
+    # Defining dict for model creation
+    hpinstance = {
+        'solver': hp.get('solver'),
+        'max_iter': hp.get('max_iter'),
+        'C': c
+    }
 
-    # Initiating the model 
-    model = LogisticRegression(**hp)
+    # Placeholders
+    recalls, precisions, aucs = [], [], []
 
-    # Training the model 
-    model.fit(X_train, y_train)
+    # Iterating over the dataset 
+    for train_index, test_index in rkf.split(X):
+        # Getting the train and test splits 
+        X_train, X_test = X.iloc[train_index], X.iloc[test_index]
+        y_train, y_test = y.iloc[train_index], y.iloc[test_index]
 
-    # Predicting 
-    yhat = model.predict(X_test)
+        # Initiating the model 
+        model = LogisticRegression(**hpinstance)
 
-    # Getting the recall and precision
-    recall, precision = recall_score(y_test, yhat), precision_score(y_test, yhat)
+        # Training the model 
+        model.fit(X_train, y_train)
 
-    # Appending to list 
-    recalls.append(recall)
-    precisions.append(precision)
+        # Predicting 
+        yhat = model.predict(X_test)
 
-# Creating the results data frame 
-valFrame = pd.DataFrame({'recall': recalls, 'precision': precisions})
+        # Getting the probabilities 
+        yhatp = [x[1] for x in model.predict_proba(X_test)]
 
-# Printing the descriptive stats 
-print(valFrame.describe())
+        # Getting the recall and precision
+        recall, precision, auc = recall_score(y_test, yhat), precision_score(y_test, yhat), roc_auc_score(y_test, yhatp)
 
-# Initiating the model 
-model = LogisticRegression(**hp)
+        # Appending to list 
+        recalls.append(recall)
+        precisions.append(precision)
+        aucs.append(auc)
+
+    # Creating the results data frame 
+    valFrame = pd.DataFrame({
+        'recall': recalls, 
+        'precision': precisions,
+        'auc': aucs
+        })
+
+    # Calculating the F1 score 
+    valFrame['f1'] = (2 * valFrame['recall'] * valFrame['precision']) / (valFrame['recall'] + valFrame['precision'])
+
+    # Calculating GINI 
+    valFrame['gini'] = 2 * valFrame['auc'] - 1
+
+    # Printing the descriptive stats 
+    print(valFrame.describe())
+
+    # Saving some hyper parameters
+    valFrame['C'] = c
+    
+    # Saving the results 
+    results = pd.concat([results, valFrame])
+
+# Aggregating and constructing the final best hyper parameter dict 
+agg = results.groupby('C', as_index=False)['gini'].mean()
+agg.sort_values('gini', ascending=False, inplace=True)
+
+# Printing out the agg results
+print(agg)
+
+hpbest = {
+        'solver': hp.get('solver'),
+        'max_iter': hp.get('max_iter'),
+        'C': agg.head(1)['C'].values[0]
+    }
+
+# Initiating the model
+model = LogisticRegression(**hpbest)
 
 # Fitting the model 
 model.fit(X, y)
